2a01bd28f6210f4571512dbba3bff3da73cad826
Nightstars <ChristZhangowner@gmail..com>
2020-12-28 17:33:52 +0800

    添加项目文件。

---
+++ WebApiDemo/Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private List<User> users;
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+            users = new List<User>
+            {
+                new User{name="张三",phone="19987654321"},
+                new User{name="李四",phone="19987654321"},
+                new User{name="赵六",phone="19987654321"}
+            };
+        }
+
+        [HttpGet]
+        public Object Get()
+        {
+            return new { status = "Success", data = users }; ;
+        }
+
+        [HttpPost]
+        public Object Add(User user)
+        {
+            users.Add(user);
+            return new  {status="Success",data=users };
+        }
+    }
+}
---
+++ WebApiDemo/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiDemo
+{
+    public class User
+    {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string  name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the phone.
+        /// </summary>
+        /// <value>
+        /// The phone.
+        /// </value>
+        public string phone { get; set; }
+    }
+}
